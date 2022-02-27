7d2e0be5cb33784d1257ee743ad3eba927228416
Wendy Wu <shadow0.0www@gmail.com>
2022-02-26 20:19:30 -0500

    comment out main.

--- assignments/my_assignment_solutions/assignment3-comp322-wendy.cpp
+++ assignments/my_assignment_solutions/assignment3-comp322-wendy.cpp
@@ -306,24 +306,24 @@ void BlackJackGame::printValue(pair<int,int> p) {
     cout << endl;
 }
 
-int main(){
-    cout << "Welcome to the Comp322 Blackjack game!" << endl << endl;
-    BlackJackGame game;
-// The main loop of the game
-    bool playAgain = true;
-    char answer = 'y';
-    while (playAgain)
-    {
-        game.play();
-// Check whether the player would like to play another round
-        cout << "Would you like another round? (y/n): ";
-        cin >> answer;
-//        cout << endl << endl;
-        playAgain = (answer == 'y' ? true : false);
-    }
-    cout <<"Gave over!";
-    return 0;
-}
+//int main(){
+//    cout << "Welcome to the Comp322 Blackjack game!" << endl << endl;
+//    BlackJackGame game;
+//// The main loop of the game
+//    bool playAgain = true;
+//    char answer = 'y';
+//    while (playAgain)
+//    {
+//        game.play();
+//// Check whether the player would like to play another round
+//        cout << "Would you like another round? (y/n): ";
+//        cin >> answer;
+////        cout << endl << endl;
+//        playAgain = (answer == 'y' ? true : false);
+//    }
+//    cout <<"Gave over!";
+//    return 0;
+//}
 
 
 
